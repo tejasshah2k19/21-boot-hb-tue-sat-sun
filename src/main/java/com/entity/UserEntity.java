fc725b14af73ef1bd84f5fda8c80c76c495957c3
Tejas <tejas14all@gmail.com>
2021-08-30 11:38:26 +0530

    hb-boot-first

---
+++ src/main/java/com/entity/UserEntity.java
@@ -0,0 +1,96 @@
+package com.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
+@Table(name="users")
+public class UserEntity {
+
+	@Id
+	@GeneratedValue
+	Long userId;
+	
+	@NotBlank(message = "Please Enter firstName")
+	String firstName;
+	
+	@Column(nullable = false,updatable = false)
+	String email;
+	
+	
+	String password;
+	
+	//@Temporal(TemporalType.TIMESTAMP)
+	//@CreatedDate
+	@CreationTimestamp
+	Date createdAt;
+	
+	//@Temporal(TemporalType.TIMESTAMP)
+	//@LastModifiedDate
+	@UpdateTimestamp
+	Date updatedAt;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	
+	
+}
